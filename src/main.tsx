990ff88cdd4e3864069bcc3d587a843074d22b57
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-20 21:20:35 +0200

    feat: add mantine provider and theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/notifications/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const theme = createTheme({
+	// fontFamily: "DM Sans, sans-serif",
+	// fontFamilyMonospace: "DM Sans, sans-serif",
+	// headings: { fontFamily: "DM Sans, sans-serif" },
+	defaultRadius: "xl",
+	primaryColor: "indigo",
+	primaryShade: 7,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<MantineProvider theme={theme}>
+			<App />
+		</MantineProvider>
+	</React.StrictMode>,
+);
