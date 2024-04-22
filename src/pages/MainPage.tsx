fa06ddeb013fbf57a81178f908fae5af5845269b
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-22 16:54:40 +0200

    style: change file name

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "@mantine/core";
+
+const MainPage = () => {
+	return (
+		<Container size={"lg"} my={"xl"}>
+			<Title>A website for my trackings</Title>
+		</Container>
+	);
+};
+
+export default MainPage;
