76b1d219d647d62b1592cc7a0290a119034d0af9
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-24 19:54:50 +0200

    feat: add products, add list component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,20 @@
 import { Container, Title } from "@mantine/core";
+import ProductsList from "components/ProductsList";
 
 const MainPage = () => {
+	const productNameArray = [
+		"decathlon",
+		"nexx",
+		"powertronic_ecu",
+		"remarkable",
+		"ruroc",
+		"toe_spacer",
+	];
+
 	return (
 		<Container size={"lg"} my={"xl"}>
 			<Title>A website for my trackings</Title>
+			<ProductsList products={productNameArray} />
 		</Container>
 	);
 };
