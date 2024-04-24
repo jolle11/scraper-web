dc1b925f29b111da7b6c774ed3eaa2a12293b878
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-24 19:55:37 +0200

    feat: add redord type

---
+++ src/types/IRecord.ts
@@ -0,0 +1,10 @@
+export interface IRecord {
+	collectionId: string;
+	collectionName: string;
+	created: string;
+	currency: string;
+	id: string;
+	price: number;
+	product: string;
+	updated: string;
+}
