8d717a7333762e26ef489956349e68b8fa5497fc
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-24 19:38:22 +0200

    feat: add dynamic imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import { Footer } from "components/Footer";
+import { Header } from "components/Header";
 
 function App() {
 	return (
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			components: "/src/components",
+			pages: "/src/pages",
+			hooks: "/src/hooks",
+			styles: "/src/styles",
+			assets: "/src/assets",
+			types: "src/types",
+		},
+	},
 });
