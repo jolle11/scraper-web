59e4466cb771e83c053c5c4c414cb7b71a8042ee
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-24 19:54:08 +0200

    fix: add properties, format data

--- src/components/PriceChart.tsx
+++ src/components/PriceChart.tsx
@@ -1,31 +1,38 @@
 import { LineChart } from "@mantine/charts";
-import CustomChartTooltip from "./CustomChartTooltip";
+import useDateFormat from "hooks/utils/useDateFormat";
+import CustomChartTooltip from "components/CustomChartTooltip";
+import { IRecord } from "types/IRecord";
 
-//TODO: define prices type
-const PriceChart = (prices: any) => {
-	// const media_line = Number.parseFloat(media_paid as string);
+const PriceChart = ({ product }: { product: IRecord[] }) => {
+	const formatDate = useDateFormat();
+
+	const data = product.map((item) => ({
+		id: item.collectionId,
+		price: item.price,
+		currency: item.currency ?? null,
+		created: formatDate(item.created),
+	}));
+
+	const YTop = Math.ceil(data[0].price / 50) * 50;
+	const lastElement = data.pop();
 	return (
 		<LineChart
-			h={200}
-			data={[...prices].reverse()}
+			h={300}
+			data={data}
 			dataKey="created"
-			series={[{ name: "price", label: "Price", color: "yellow.9" }]}
-			// referenceLines={[
-			// 	{
-			// 		y: media_line,
-			// 		label: `Media paid ${media_paid}€`,
-			// 		color: "indigo.6",
-			// 	},
-			// ]}
+			series={[{ name: "price", label: "Price", color: "green.9" }]}
+			activeDotProps={{ r: 4, strokeWidth: 2, fill: "#fff" }}
+			dotProps={{ r: 2, strokeWidth: 1 }}
+			strokeWidth={1}
+			unit={lastElement?.currency}
+			curveType="stepAfter"
+			tooltipAnimationDuration={200}
+			yAxisProps={{ domain: [0, YTop + 50] }}
 			tooltipProps={{
 				content: ({ label, payload }) => (
 					<CustomChartTooltip label={label} payload={payload} />
 				),
 			}}
-			activeDotProps={{ r: 7, strokeWidth: 2, fill: "#fff" }}
-			strokeWidth={1.5}
-			unit="€"
-			curveType="bump"
 		/>
 	);
 };
