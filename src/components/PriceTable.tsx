ed79dc5bfd11f4295ed29847501f6ef0d8e3cd37
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-04-24 23:03:14 +0200

    feat: add scroll container, change product map, add caption

--- src/components/PriceTable.tsx
+++ src/components/PriceTable.tsx
@@ -1,45 +1,56 @@
-import { Table, Title } from "@mantine/core";
+import { Center, Table, Text } from "@mantine/core";
 import useDateFormat from "hooks/utils/useDateFormat";
 import { IRecord } from "types/IRecord";
 
 const PriceTable = ({ product }: { product: IRecord[] }) => {
 	const formatDate = useDateFormat();
+	const variableHeight = 35 * product.length + 70;
+
 	return (
 		<>
-			<Title size={"h3"}>Last 20 prices</Title>
-			<Table striped highlightOnHover>
-				<Table.Thead>
-					<Table.Tr>
-						<Table.Th style={{ textAlign: "center" }}>Price</Table.Th>
-						<Table.Th style={{ textAlign: "center" }}>Currency</Table.Th>
-						<Table.Th style={{ textAlign: "center" }}>Created</Table.Th>
-					</Table.Tr>
-				</Table.Thead>
-				<Table.Tbody>
-					{product?.map((p, index) => (
-						<Table.Tr key={p.id}>
-							<Table.Td
-								style={{ textAlign: "center" }}
-								display={`${index > 20 ? "none" : ""}`}
-							>
-								{p.price}
-							</Table.Td>
-							<Table.Td
-								style={{ textAlign: "center" }}
-								display={`${index > 20 ? "none" : ""}`}
-							>
-								{p.currency}
-							</Table.Td>
-							<Table.Td
-								style={{ textAlign: "center" }}
-								display={`${index > 20 ? "none" : ""}`}
-							>
-								{formatDate(p.created)}
-							</Table.Td>
+			<Table.ScrollContainer
+				minWidth={500}
+				h={product.length < 16 ? variableHeight : 500}
+				my={20}
+				type="native"
+				style={{
+					borderColor: "#CED4DA",
+					borderWidth: "1px",
+					borderStyle: "solid",
+				}}
+			>
+				<Table
+					striped
+					stickyHeader
+					stickyHeaderOffset={-5}
+					withTableBorder
+					highlightOnHover
+				>
+					<Table.Thead>
+						<Table.Tr h={50}>
+							<Table.Th style={{ textAlign: "center" }}>Price</Table.Th>
+							<Table.Th style={{ textAlign: "center" }}>Currency</Table.Th>
+							<Table.Th style={{ textAlign: "center" }}>Created</Table.Th>
 						</Table.Tr>
-					))}
-				</Table.Tbody>
-			</Table>
+					</Table.Thead>
+					<Table.Tbody>
+						{product?.map((p) => (
+							<Table.Tr key={p.id}>
+								<Table.Td style={{ textAlign: "center" }}>{p.price}</Table.Td>
+								<Table.Td style={{ textAlign: "center" }}>
+									{p.currency}
+								</Table.Td>
+								<Table.Td style={{ textAlign: "center" }}>
+									{formatDate(p.created)}
+								</Table.Td>
+							</Table.Tr>
+						))}
+					</Table.Tbody>
+				</Table>
+			</Table.ScrollContainer>
+			<Center>
+				<Text>Some elements may be missing or without currency</Text>
+			</Center>
 		</>
 	);
 };
